13202b6bb7df39aeb88a73c064d0503e95e6843a
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-12-18 20:36:03 +0200

    feat: write to .toml the added dep

--- lunaship_core/src/add.rs
+++ lunaship_core/src/add.rs
@@ -1,11 +1,19 @@
 use crate::error::LunashipError;
 use crate::pathbuf;
+use crate::specs::DependencySpecs;
 use crate::specs::Specs;
 
 pub fn add_dependency(alias: &str, git: &str) -> Result<(), LunashipError> {
-    println!("Adding dependency: {} from {}", alias, git);
-    let raw_spec = std::fs::read_to_string(pathbuf![".", "lunaship.toml"])?;
-    let specs = toml::from_str::<Specs>(&raw_spec)?;
-    println!("{specs:#?}");
+    let spec_path = pathbuf![".", "lunaship.toml"];
+    let raw_spec = std::fs::read_to_string(&spec_path)?;
+    let mut specs = toml::from_str::<Specs>(&raw_spec)?;
+    specs.add_dependency(
+        alias.to_string(),
+        DependencySpecs {
+            git: git.to_string(),
+        },
+    );
+    let raw_specs = toml::to_string(&specs)?;
+    std::fs::write(spec_path, raw_specs)?;
     Ok(())
 }
--- lunaship_core/src/specs.rs
+++ lunaship_core/src/specs.rs
@@ -1,12 +1,25 @@
 use serde::Deserialize;
 use serde::Serialize;
+use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Specs {
     project: ProjectSpecs,
+    dependencies: HashMap<String, DependencySpecs>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ProjectSpecs {
     name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DependencySpecs {
+    pub git: String,
+}
+
+impl Specs {
+    pub fn add_dependency(&mut self, alias: String, dep_spec: DependencySpecs) {
+        self.dependencies.insert(alias, dep_spec);
+    }
 }
